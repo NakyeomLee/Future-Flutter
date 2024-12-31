cd3fb83273e136659f04d9f79252db809bca5d50
ãNakyeomLee <dlsrud0620@naver.com>
2024-12-31 15:30:51 +0900

    컬렉션 반환받도록 통신

---
+++ lib/post_list_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mockapp/post_body.dart';
+import 'package:mockapp/post_list_body.dart';
+
+
+class PostListPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PostListBody(),
+    );
+  }
+}
--- lib/post_repository.dart
+++ lib/post_repository.dart
@@ -1,7 +1,7 @@
 // SRP : 데이터를 가져오는 곳 (휴대폰 디바이스(파일), 휴대폰 DB, Firebase(외부서버), 내서버, 공공데이터서버)
 
 import 'package:dio/dio.dart';
-import 'package:logger/logger.dart';
+import 'package:mockapp/post_list_page_vm.dart';
 import 'package:mockapp/post_page_vm.dart';
 
 import 'http_util.dart';
@@ -18,9 +18,9 @@ class PostRepository {
   }
 
   // 컬렉션을 반환하는 경우
-  Future<List<Post>> getPostList() async{
-    Response response = await dio.get("/posts"); // Map으로 받음(외우기)
+  Future<List<Post2>> getPostList() async{
+    Response response = await dio.get("/posts"); // Map으로 받음 (외우기)
     List<dynamic> list = response.data;
-    return list.map((e) => Post.fromMap(e)).toList();
+    return list.map((e) => Post2.fromMap(e)).toList();
   }
 }
